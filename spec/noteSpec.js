512cccd7fe39102dc1b33ab1ee54c7daa2af7c17
Radu Necsanu <necsanu_radu@yahoo.com>
2021-09-07 16:43:02 +0100

    testing enviroment ready

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
 describe("Note class", () => {
   
   it("some and some", () => {
@@ -5,4 +6,10 @@ describe("Note class", () => {
     expect(1).toEqual(1);
   })
 
+  it("some and some", () => {
+    
+    expect(1).toEqual(2);
+  })
+
+});
 })
