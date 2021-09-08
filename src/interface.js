705d781dca285fd59a909383799fbd9184fe9c40
Ahsan <rasul.ahsan@gmail.com>
2021-09-08 16:24:30 +0100

    added individual index to the dynamically created divs

--- src/interface.js
+++ src/interface.js
@@ -1,19 +1,18 @@
-window.onload = function(){
-    noteClass = new Note();
-    document.querySelector('#addNote').addEventListener('click',() => {
-      let message = document.querySelector('#noteValue').value;
-      document.querySelector('#noteValue').value = "";
-      noteClass.createNote(message);
-    })
+window.onload = function () {
+	noteClass = new Note();
+	document.querySelector("#addNote").addEventListener("click", () => {
+		let message = document.querySelector("#noteValue").value;
+		document.querySelector("#noteValue").value = "";
+		noteClass.createNote(message);
+	});
+};
+
+function noteToHtml(message, index) {
+	div = document.createElement("div");
+	div.id = index;
+	div.innerHTML = message;
+	//div.setAttribute('onclick', )
+	document.body.insertAdjacentElement("beforeend", div);
 }
 
-function noteToHtml(message){
-    div = document.createElement('div');
-    div.innerHTML = message;
-    //div.setAttribute('onclick', )
-    document.body.insertAdjacentElement('beforeend', div);
-}
-
-function noteToMainView(message){
-
-}
+function noteToMainView(message) {}
--- src/note.js
+++ src/note.js
@@ -1,30 +1,31 @@
-class Note{
-      constructor(){
-        this.notePad = [];
-      }
+class Note {
+	constructor() {
+		this.notePad = [];
+	}
 
-      createNote(message){
-        if(message.replace(/ /g, '') === "") return;
-        let note = new Message(message.trim());
-        this.notePad.push(note);
-        this.listNotes(note);
-      }
+	createNote(message) {
+		if (message.replace(/ /g, "") === "") return;
+		let note = new Message(message.trim());
+		this.notePad.push(note);
+		this.listNotes(note);
+	}
 
-      listNotes(note){
-        let message;
-        if(note ==null){
-            //load all the notes
-            this.notePad.forEach((thisNote) => {
-              message = thisNote.getNoteTitle();
-              noteToHtml(message);
-            })
-        }else{
-            message = note.getNoteTitle();
-            //load just this note
-            noteToHtml(message);
-        }
-      }
-
-
-  }
-
+	listNotes(note) {
+		let message;
+		let index;
+		if (note == null) {
+			//load all the notes
+			this.notePad.forEach((thisNote) => {
+				console.log("load all notes");
+				message = thisNote.getNoteTitle();
+				noteToHtml(message, index);
+			});
+		} else {
+			index = this.notePad.length - 1;
+			message = note.getNoteTitle();
+			//load just this note
+			console.log("load one note");
+			noteToHtml(message, index);
+		}
+	}
+}
